cd71cdb2afaf2845d2725d80ac06bf30d78f24eb
edson <edson@172.20.130.194>
2017-11-29 11:44:18 -0300

    Implementacao Inicial

---
+++ src/main/java/br/com/ufba/grouprecommendation/model/Usuario.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.ufba.grouprecommendation.model;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author edson
+ */
+public class Usuario {
+    private String nome;
+    private HashMap<Double, Integer> qtdLikeTemp;
+    private HashMap<Double, Integer> qtdLikeLumino;
+    private HashMap<Double, Integer> qtdLikeSom;
+}
